4e6c1202f0fc218d85e93abc0e1de3ab485be0c1
Mogyorósi Bálint <mogyorosibalint@Mogyorosi-MacBook-Air.local>
2018-08-13 12:42:17 +0200

    PokerClient class is refactored, and all the tests are correct

--- src/PokerClient.java
+++ src/PokerClient.java
@@ -1,54 +1,29 @@
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class PokerClient {
 
-    public Card card1;
-    public Card card2;
-    public Card card3;
-    public Card card4;
-    public Card card5;
+    private List<Card> ownCards = new ArrayList<>();
 
-    public PokerClient(String p1, String p2, String p3, String p4, String p5) {
-        this.card1 = new Card(p1.toUpperCase());
-        this.card2 = new Card(p2.toUpperCase());
-        this.card3 = new Card(p3.toUpperCase());
-        this.card4 = new Card(p4.toUpperCase());
-        this.card5 = new Card(p5.toUpperCase());
+    public PokerClient(String code1, String code2, String code3, String code4, String code5) {
+        ownCards.addAll(Arrays.asList(new Card(code1), new Card(code2), new Card(code3), new Card(code4), new Card(code5)));
     }
 
-    public boolean highestCardIsMine(String p1, String p2, String p3, String p4, String p5) {
-        Card hc = new Card("s2");
-        List<Card> o = new ArrayList<Card>();
-        List<Card> m = new ArrayList<Card>();
-        o.add(new Card(p1.toUpperCase()));
-        o.add(new Card(p2.toUpperCase()));
-        o.add(new Card(p3.toUpperCase()));
-        o.add(new Card(p4.toUpperCase()));
-        o.add(new Card(p5.toUpperCase()));
-        m.add(card1);
-        m.add(card2);
-        m.add(card3);
-        m.add(card4);
-        m.add(card5);
+    public boolean highestCardIsMine(String code1, String code2, String code3, String code4, String code5) {
+        List<Card> cards = new ArrayList<>(Arrays.asList(new Card(code1), new Card(code2), new Card(code3), new Card(code4), new Card(code5)));
+        cards.addAll(ownCards);
 
-        for (int i = 0; i < o.size(); i++) {
-            Card mc = m.get(i);
-            for (int j = 0; j < o.size(); j++) {
-                Card oc = o.get(j);
-                if (oc.getValue() > mc.getValue()) {
-                    if (oc.getValue() > hc.getValue()) {
-                        hc = oc;
-                    }
-                } else {
-                    if (mc.getValue() > hc.getValue()) {
-                        hc = mc;
-                    }
-                }
+        Card highestCard = cards.get(0);
+        Card currentCard;
+        for (int i = 1; i < cards.size(); i++) {
+            currentCard = cards.get(i);
+            if (currentCard.getValue() > highestCard.getValue()) {
+                highestCard = currentCard;
             }
         }
 
-        return m.contains(hc);
+        return ownCards.contains(highestCard);
     }
 
 }
